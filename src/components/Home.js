60598d7c9b861581fb809edfc3cb42a53bf5f4d9
Rahul Sharma <rahulsharma15j@gmail.com>
2022-03-21 16:42:30 +0530

    heder added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,38 @@
 import styled from "styled-components";
 
 const Home = (props) => {
-  return <div>Home</div>;
+  return (
+    <Container>
+      <Section>
+        <h5>
+          <a>Hiring in a hurry? - </a>
+        </h5>
+        <p>
+          Find talented pros in record time with Upwork and keep business
+          moving.
+        </p>
+      </Section>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  padding-top: 52px;
+  max-width: 100%;
+`;
+const Content = styled.div`
+  max-width: 128px;
+  margin-left: auto;
+  margin-right: auto;
+`;
+const Section = styled.section`
+  min-height: 50px;
+  padding: 16px 0;
+  box-sizing: content-box;
+  text-align: center;
+  text-decoration: underline;
+  display: flex;
+  justify-content: center;
+`;
 
 export default Home;
