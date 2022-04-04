18831b972afed87a3f0a7be42f423b64559dc150
Rahul Sharma <rahulsharma15j@gmail.com>
2022-04-04 16:45:40 +0530

    google auth added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import "./App.css";
 import Login from "./components/Login";
@@ -8,19 +8,15 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Routes>
-          <Route exact path="/" element={<Login />}></Route>
-          <Route
-            path="/home"
-            element={
-              <>
-                {" "}
-                <Header />
-                <Home />
-              </>
-            }
-          ></Route>
-        </Routes>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route path="/home">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
       </Router>
     </div>
   );
---
+++ src/actions/index.js
@@ -0,0 +1,30 @@
+import { auth, provider } from "../firebase";
+import { SET_USER } from "./actionType";
+
+export const setUser = (payload) => ({
+  type: SET_USER,
+  user: payload,
+});
+
+export function signInAPI() {
+  return (dispatch) => {
+    auth
+      .signInWithPopup(provider)
+      .then((payload) => {
+        dispatch(setUser(payload.user));
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+}
+
+export function getUserAuth() {
+  return (dispatch) => {
+    auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        dispatch(setUser(user));
+      }
+    });
+  };
+}
